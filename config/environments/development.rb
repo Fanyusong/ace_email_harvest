2c2928afc538a2ef72d131ea85134436a6df7405
FanYuSong <kyo.shenshie@gmail.com>
2017-12-24 16:06:12 +0700

    no message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
+  # This option may cause session[:]ignificant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
 
