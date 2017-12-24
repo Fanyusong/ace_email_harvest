68d62cd2f8a95e74c99211496ad6ffe5b0bba35a
FanYuSong <kyo.shenshie@gmail.com>
2017-12-24 21:25:26 +0700

    no message

--- app/mailers/campaign_email_mailer.rb
+++ app/mailers/campaign_email_mailer.rb
@@ -1,5 +1,4 @@
 class CampaignEmailMailer < ApplicationMailer
-  helper :email_campaign_mailer
   layout 'mailer'
   def action_send_email(from, to , subject, body )
     track extra: {campaign_id: 1, to: to}
