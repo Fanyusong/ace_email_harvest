d1074f9569287312e82afb52f39c087df1fe5889
FanYuSong <kyo.shenshie@gmail.com>
2017-12-24 14:36:53 +0700

    Config sidekiq

--- config/puma.rb
+++ config/puma.rb
@@ -3,25 +3,23 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
-
-port        ENV.fetch("PORT") { 3000 }
-
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 preload_app!
 
-on_worker_boot do
-  @sidekiq_pid ||= spawn('bundle exec sidekiq -t 25')
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+rackup      DefaultRackup
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+before_fork do
+  puts "Puma master process about to fork. Closing existing Active record connections."
+  ActiveRecord::Base.connection.disconnect!
 end
 
-on_restart do
-  Sidekiq.redis.shutdown { |conn| conn.close }
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
 end
 
 plugin :tmp_restart
