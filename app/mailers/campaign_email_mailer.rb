4adb0a0e90f3251fbdcc549a3dfc8182713d5f7d
FanYuSong <kyo.shenshie@gmail.com>
2017-12-24 16:22:54 +0700

    no message

--- app/mailers/campaign_email_mailer.rb
+++ app/mailers/campaign_email_mailer.rb
@@ -5,6 +5,7 @@ class CampaignEmailMailer < ApplicationMailer
     mail(from: from,
          bcc: to,
          subject: subject,
-         body: body)
+         body: body,
+         content_type: "text/html")
   end
 end
