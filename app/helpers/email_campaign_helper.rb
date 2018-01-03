6e37e6a9b5865a437083d51032523bf9d6fcd4bb
FanYuSong <kyo.shenshie@gmail.com>
2018-01-03 19:45:33 +0700

    Add admin

--- app/helpers/email_campaign_helper.rb
+++
@@ -1,2 +0,0 @@
-module EmailCampaignHelper
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221124852) do
+ActiveRecord::Schema.define(version: 20180102150636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,10 +34,6 @@ ActiveRecord::Schema.define(version: 20171221124852) do
     t.string "address"
     t.integer "user_type"
     t.datetime "birthday"
-    t.string "confirmation_token"
-    t.string "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.datetime "unconfirmed_email"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -83,6 +79,39 @@ ActiveRecord::Schema.define(version: 20171221124852) do
     t.integer "user_id"
   end
 
+  create_table "list_email_campaigns", force: :cascade do |t|
+    t.string "email"
+    t.integer "email_campaign_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sent_email_opens", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "ip_address"
+    t.string "opened"
+    t.integer "campaign_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sent_emails", force: :cascade do |t|
+    t.string "name"
+    t.integer "campaign_id"
+    t.string "email"
+    t.datetime "sent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_logs", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "action"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -106,13 +135,9 @@ ActiveRecord::Schema.define(version: 20171221124852) do
     t.string "address"
     t.integer "user_type"
     t.datetime "birthday"
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.text "tokens"
-    t.json "email_file"
+    t.boolean "is_lock", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "email_campaigns", "users"
