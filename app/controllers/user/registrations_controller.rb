3a5707dbedfd3d18763139c72699c17f571b7571
FanYuSong <kyo.shenshie@gmail.com>
2018-01-13 15:04:25 +0700

    Fix user sign_up

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -3,4 +3,13 @@
 class User::RegistrationsController < Devise::RegistrationsController
   layout 'application_user'
 
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :address, :birthday, :email, :password, :password_confirmation, :current_password)
+  end
 end
